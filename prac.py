488a855c5591252493256eed09f56b033ed7ebf8
Mb4y4 <d.sommy@gmail.com>
2023-09-02 13:02:02 +0300

    test

---
+++ prac.py
@@ -0,0 +1,6 @@
+my_array = [1, 2, 3, 4, 5, 6, 7, 8]
+
+print(my_array[0])
+# prints the first element of my array (index starts at zero)
+
+for i in range(0,len(my_array))
---
+++ script.js
@@ -0,0 +1,18 @@
+/*//const age = [7, 19, 2, 90, 24, 30];
+
+//console.log(age[0]) //Output: 7
+//console.log(age[1]) //Output: 19
+//console.log(age[2])  //Output: 2
+//console.log(age[3])  //Output: 90*/
+
+//These 2 'for' ways will print all ages separately to the console.
+const age = [7, 19, 2, 90, 24, 30];
+
+for (let i=0;i<age.length;++i){
+    console.log(age[i]
+    )
+    }
+for(let i = 0; i < age.length; i++)
+{
+    console.log(age[i]);
+}
